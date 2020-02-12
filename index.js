68158bfdacc9808e8a6213576e9eb3e0ee2cd2eb
Kenny Lam <kinwai.lam730@gmail.com>
2020-02-12 16:21:40 -0500

    Added preliminary questions

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      message: "What's your GitHub username?",
+      name: "username",
+    },
+    {
+      message: "What's the name of your project?",
+      name: "projectname",
+    },
+    {
+      message: "Description of your project?",
+      name: "description",
+    },
+    {
+      message: "What do you need to install to make your program work?",
+      name: "installation"
+    },
+    {
+      message: "What kind of usage does your program have?",
+      name: "usage"
+    },
+    {
+      message: "Do you have anyone to give credit to?",
+      name: "credits"    
+    },
+    {
+      message: "Do you have any license on your project?",
+      name: "license"
+    }
+  ]).then(function(response){
+      console.log (response)
+  })
